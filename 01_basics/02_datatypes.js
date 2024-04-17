b607dea2cbf39587f6f810205ef10db90fd232da
xDevanshu-Garg <167272173+xDevanshu-Garg@users.noreply.github.com>
2024-04-17 06:57:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+ "use strict"; //treat all JS code as a newer version
+
+//  alert("hello") we are using nodejs not browser
+
+console.log(3+3); 
+ 
+
+let name = "Devanshu"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+// boolean => true/false
+// null => standalone value/khali
+// undefined => abhi value assign nhi hui h
+// symbol => define uniqnes 
+
+// object
+
+console.log(typeof "Devanshu");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);

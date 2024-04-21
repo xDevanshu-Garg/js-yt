384944df55e560bcabf2ac8981ab43f8624f89eb
xDevanshu-Garg <167272173+xDevanshu-Garg@users.noreply.github.com>
2024-04-21 12:24:57 +0000

    half nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,13 @@
+const score = 400
+console.log(score);
+
+let balance = new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(typeof(balance));
+
+balance = 100.24846
+console.log(balance.toFixed(2)); //used to store specific amount of decimal values 
+
+const otherNumber = 43.5623
+// console.log(otherNumber.toPrecision(3);
